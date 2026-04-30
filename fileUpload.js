2ae22b18bf31587e0819134f03992d9cd23c10e6
Mintesnot Girma <mintesnotgirma16@gmail.com>
2026-04-30 04:28:58 -0400

    updated cloudinary

--- fileUpload.js
+++ fileUpload.js
@@ -3,7 +3,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
